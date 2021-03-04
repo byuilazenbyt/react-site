e7ed399620a0f91a5290c561fe027c673c9707fd
Tyler Lazenby <laz06001@byui.edu>
2021-03-04 15:55:29 -0700

    basic structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Header } from "./template/Header";
 import { SideBar } from "./template/Sidebar";
 import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
+import Banner from './img/iStock-1210526452.jpg';
 
 function App() {
   return (
@@ -10,6 +12,12 @@ function App() {
        <div className="container-fluid">
            <Row>
                <SideBar/>
+               <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                   <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                       <h1 className="h2">Home</h1>
+                   </div>
+                   <Image className="img-fluid rounded shadow" src={Banner} alt="guy holding phone"/>
+               </main>
            </Row>
        </div>
       </div>
--- src/template/Header.js
+++ src/template/Header.js
@@ -8,13 +8,13 @@ export class Header extends React.Component {
         return (
             <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
                 <Navbar.Brand href="#home" className="col-md-3 col-lg-2 me-0 px-3">
-                    Tyler Lazenby
+                    All Things Web Dev
                 </Navbar.Brand>
                 <Navbar.Toggle className="position-absolute d-md-none" />
                 <FormControl type="text" className="form-control-dark w-100" placeholder="Search" aria-label="Search"/>
-                <ul className="navbar-nav px3">
+                <ul className="navbar-nav px-3">
                     <Nav.Item className="text-nowrap">
-                        <Nav.Link href="#">Sign Out</Nav.Link>
+                        <Nav.Link href="#">Sign In</Nav.Link>
                     </Nav.Item>
                 </ul>
             </header>
